5357a9ae65a1af0f82e76212d8546b060391a428
chilam0103 <chilam9x@gmail.com>
2020-05-13 14:22:01 +0700

    phân chia redux căn bản

--- src/trainning/demo.js
+++ src/trainning/demo.js
@@ -1,45 +1,13 @@
 import { createStore } from "redux";
-
-var initialState = {
-  status: false,
-  sort: {
-    by: "name",
-    value: 1, //1: tang, -1: giam
-  },
-};
-
-var myReducer = (state = initialState, action) => {
-  if (action.type === "TOGGLE_STATUS") {
-    state.status = !state.status;
-    return state;
-  }
-  if (action.type === "SORT") {
-    var { by, value } = action.sort;
-    var { status } = state;
-    return {
-      status: status,
-      sort: {
-        by: by,
-        value: value,
-      },
-    };
-  }
-  return state;
-};
+import {status,sort} from "./actions/index";
+import myReducer from "./reducers/index";
 const store = createStore(myReducer);
 console.log('default',store.getState());
-
 //thực hiện công việc thay đổi status
-var action = { type: "TOGGLE_STATUS" };
-store.dispatch(action);
+store.dispatch(status());
 console.log("toogle", store.getState());
 //thực hiện công việc sắp xếp name Z-A
-var sort_action = {
-  type: "SORT",
-  sort: {
-    by: "name",
-    value: -1,
-  },
-};
-store.dispatch(sort_action);
+store.dispatch(sort({
+    by: "name", value:-1
+}));
 console.log("sort", store.getState());
---
+++ src/trainning/reducers/sort.js
@@ -0,0 +1,13 @@
+var initialState = {
+  by: "name",
+  value: 1, //1: tang, -1: giam
+};
+
+var myReducer = (state = initialState, action) => {
+  if (action.type === "SORT") {
+    var { by, value } = action.sort;
+    return { by, value };
+  }
+  return state;
+};
+export default myReducer;
